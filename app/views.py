4576a70951bcc52439d49159e2759dc09adef02d
deepa28li <swaralipatil2000@gmail.com>
2024-01-31 10:47:41 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,5 +4,23 @@ from django.shortcuts import render
 from app.models import *
 def equijoin(request):
     EMPOBJECTS=Emp.objects.select_related('deptno').all()
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(hiredate__year=2024)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(hiredate__year=2023,sal__gt=2500)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno=10)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno__dname='Sales')
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(mgr__isnull=True)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=False)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(comm__isnull=True)
+
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(mgr__isnull=True,deptno=10)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(sal__lt=2000,deptno=20)
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(ename='Smith')
+    EMPOBJECTS=Emp.objects.select_related('deptno').all()[:5:]
+    EMPOBJECTS=Emp.objects.select_related('deptno').all()[2:5:]
+    
+    EMPOBJECTS=Emp.objects.select_related('deptno').filter(deptno__dname='Accounting')
+    
+    
+    # EMPOBJECTS=Emp.objects.select_related('deptno').all()
     d={'EMPOBJECTS':EMPOBJECTS}
     return render(request,'equijoin.html',d)
